6af41d7678c90407e9d5f86265e9ba59ff85d3df
thejustvic <thejustvic@gmail.com>
2020-12-31 20:32:58 +0200

    cleanup unnecessary files

--- server/index.js
+++ server/index.js
@@ -1,9 +1,6 @@
-require('ignore-styles')
-
 require('@babel/register')({
   ignore: [/(node_modules)/],
-  presets: ['@babel/preset-env', '@babel/preset-react'],
-  plugins: ["babel-plugin-styled-components"]
+  presets: ['@babel/preset-env', '@babel/preset-react']
 })
 
 require('./server')
